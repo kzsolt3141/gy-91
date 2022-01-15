d46b5ea583c3451e08ac91f38b13c2df9a3bb61f
Zsolt <kzsolt3141@gmail.com>
2022-01-15 23:22:24 +0200

    add MPU and BMP init code in main

--- test/main.c
+++ test/main.c
@@ -1,6 +1,7 @@
 #include "uart.h"
 #include "twi.h"
 #include "MPU9250.h"
+#include "BMP280.h"
 
 #include <stdio.h>
 #include <avr/io.h>
@@ -8,7 +9,6 @@
 
 typedef struct USART_RXC_cb_ctx_t {
     uint8_t rx;
-    int cnt;
 } USART_RXC_cb_ctx;
 
 /**
@@ -21,21 +21,6 @@ static void USART_RXC_cb_handle(void* ctx) {
     USART_RXC_cb_ctx* t_ctx = (USART_RXC_cb_ctx*)ctx;
 
     t_ctx->rx = UDR;
-    t_ctx->cnt++;
-}
-
-typedef struct TWI_cb_ctx_t {
-    int cnt;
-}TWI_cb_ctx;
-
-/**
- * TWI interrupt callback handle
- * @param[inout] ctx user data for interrupt callback
- */
-static void TWI_cb_handle(void* ctx) {
-    TWI_cb_ctx* t_ctx = (TWI_cb_ctx*)ctx;
-
-    t_ctx->cnt++;
 }
 
 int main(void) {
@@ -56,8 +41,6 @@ int main(void) {
     // TWI init
     //-------------------------------
     uint8_t slave_addr;
-    TWI_cb_ctx twi_ctx = {};
-    regiter_TWI_isr_cb(TWI_cb_handle, &twi_ctx);
 
     TWI_init(TWI_PS_1, 2);
 
@@ -67,7 +50,6 @@ int main(void) {
     if (sts == 0xFF) printf("TWI error\n");
 
     printf("TWI sniff: slaves:%d fist slave addr:0x%02x\n", sts, slave_addr);
-    printf("total number of TWI interrupts: %d\n", twi_ctx.cnt);
 
     // MPU9250 init
     //-------------------------------
@@ -82,28 +64,24 @@ int main(void) {
 
     MPU9250_data mpu_data = {};
 
-    int16_t min_mag[3] = {0};
-    int16_t max_mag[3] = {0};
+    // BMP280 init
+    //-------------------------------
+    register_BMP_cb(TWI_write_reg, TWI_read_reg_burst);
 
-    printf("MPU9250 getting magnetometer offsets...\n");
+    sts = BMP280_init();
+    if (sts) return sts;
 
-    while (!USART_RXC_ctx.cnt) {
-        MPU9250_get_data(&mpu_data);
+    printf("BMP280 init done\n");
 
-        if (mpu_data.mag[0] < min_mag[0]) min_mag[0] = mpu_data.mag[0];
-        if (mpu_data.mag[0] > max_mag[0]) max_mag[0] = mpu_data.mag[0];
-        if (mpu_data.mag[1] < min_mag[1]) min_mag[1] = mpu_data.mag[1];
-        if (mpu_data.mag[1] > max_mag[1]) max_mag[1] = mpu_data.mag[1];
-        if (mpu_data.mag[2] < min_mag[2]) min_mag[2] = mpu_data.mag[2];
-        if (mpu_data.mag[2] > max_mag[2]) max_mag[2] = mpu_data.mag[2];
-    }
-
-    printf("calib: %d %d %d %d %d %d\n", min_mag[0], max_mag[0], min_mag[1], max_mag[1], min_mag[2], max_mag[2]);
+    BMP280_final bmp_data = {};
 
     while(1) {
+        
         MPU9250_get_data(&mpu_data);
+        BMP280_get_data(&bmp_data);
         // printf("%05d\n", mpu_data.tmp);
-        printf("%05d, %05d, %05d\n", mpu_data.acc[0], mpu_data.acc[1], mpu_data.acc[2]);
+        printf("%05d, %05d, %05d   ....  ", mpu_data.acc[0], mpu_data.acc[1], mpu_data.acc[2]);
+        printf("%d, %d\n", (int16_t)(bmp_data.temp/256/100), (int16_t)(bmp_data.baro/256/100));
         // printf("%05d, %05d, %05d\n", mpu_data.gyro[0], mpu_data.gyro[1], mpu_data.gyro[2]);
         // printf("%05d, %05d, %05d\n", mpu_data.mag[0] - (min_mag[0] + max_mag[0]) / 2, mpu_data.mag[1] -  (min_mag[1] + max_mag[1]) / 2, mpu_data.mag[2] - (min_mag[2] + max_mag[2]) / 2);
 
