8fded0f4e4c58380b7e3ca0a5c387a12e97f5b0c
Zsolt <kzsolt3141@gmail.com>
2022-01-15 23:57:43 +0200

    comments and spaces..

--- test/main.c
+++ test/main.c
@@ -36,20 +36,9 @@ int main(void) {
 
     USART_init(baud_rate);
 
-    printf("Init Done UART baud: %u\n", (uint16_t)baud_rate);
-
     // TWI init
     //-------------------------------
-    uint8_t slave_addr;
-
     TWI_init(TWI_PS_1, 2);
-
-    printf("TWI init done\n");
-
-    sts = TWI_sniff(0x00, 0x7F, &slave_addr);
-    if (sts == 0xFF) printf("TWI error\n");
-
-    printf("TWI sniff: slaves:%d fist slave addr:0x%02x\n", sts, slave_addr);
 
     // MPU9250 init
     //-------------------------------
@@ -79,8 +68,9 @@ int main(void) {
         
         MPU9250_get_data(&mpu_data);
         BMP280_get_data(&bmp_data);
+
         // printf("%05d\n", mpu_data.tmp);
-        printf("%05d, %05d, %05d   ....  ", mpu_data.acc[0], mpu_data.acc[1], mpu_data.acc[2]);
+        printf("%05d, %05d, %05d ", mpu_data.acc[0], mpu_data.acc[1], mpu_data.acc[2]);
         printf("%d, %d\n", (int16_t)(bmp_data.temp/256/100), (int16_t)(bmp_data.baro/256/100));
         // printf("%05d, %05d, %05d\n", mpu_data.gyro[0], mpu_data.gyro[1], mpu_data.gyro[2]);
         // printf("%05d, %05d, %05d\n", mpu_data.mag[0] - (min_mag[0] + max_mag[0]) / 2, mpu_data.mag[1] -  (min_mag[1] + max_mag[1]) / 2, mpu_data.mag[2] - (min_mag[2] + max_mag[2]) / 2);
